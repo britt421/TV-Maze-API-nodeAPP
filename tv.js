dc1eb132840015e839422dac8a1df3d569d5bda1
mevpadrino <manuelman.mev@gmail.com>
2019-06-26 14:18:40 -0500

    actor info

--- tv.js
+++ tv.js
@@ -11,7 +11,7 @@ function output(info){
 
 var TV = function(){
   this.findShow = function(show){
-    var URL = "https://api.tvmaze.com/singlesearch/shows?q=" + show
+    var URL = "https://api.tvmaze.com/singlesearch/shows?q=" + show;
 
     axios.get(URL).then(function(response){
       //console.log(response);
@@ -25,7 +25,20 @@ var TV = function(){
 
     })
   }
-  this.findActor = function(){
+  this.findActor = function(actor){
+    var URL = "http://api.tvmaze.com/search/people?q=" + actor;
+
+    axios.get(URL).then(function(response){
+      // console.log(response.data[0]);
+       output(`
+      Actor Name: ${response.data[0].person.name}
+      birthday: ${response.data[0].person.birthday}
+      gender: ${response.data[0].person.gender}
+      country: ${response.data[0].person.country.name}
+       -----------------------------------
+       `);
+
+    })
   }
 }
 
