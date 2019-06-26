681cba3f9356cdcd41da785208c2faed24295a10
mevpadrino <manuelman.mev@gmail.com>
2019-06-26 14:02:36 -0500

    it works perfect

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+var TV = require("./tv");
 
+// Create a new TV object
+var tv = new TV();
 
+// Grab search command line argument
+var search = process.argv[2];
+// Joining the remaining arguments since an actor or tv show name may contain spaces
+var term = process.argv.slice(3).join(" ");
 
+// By default, if no search type is provided, search for a tv show
+if (!search) {
+  search = "show";
+}
 
-// Variables for dependencies
-var fs = require("fs");
-var axios = require("axios");
-var moment = require("moment");
+// By default, if no search term is provided, search for "Andy Griffith"
+if (!term) {
+  term = "Andy Griffith";
+}
 
-// Variable for user commands
-var command = process.argv[2];
-console.log(command);
-
-// Variable for user data inputs. Joins multiple words into index[3].
-var userInput = process.argv.splice(3).join(" ");
-console.log(userInput);
-
-if (!userInput) {
-        userInput = "andy griffith";
-        movie(userInput);
+// Print whether searching for a show or actor, print the term as well
+if (search === "show") {
+  console.log("Searching for TV Show");
+  tv.findShow(term);
+} else {
+  tv.findActor(term);
+  console.log("Searching for TV Actor");
 }
